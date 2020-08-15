697f1e063bbff989c0da996e60977932f00a4b97
Yun Hsiao Wu <inheritorofgalaxy@gmail.com>
2020-08-15 10:45:34 +0800

    better looking

--- assets/consumer.ts
+++ assets/consumer.ts
@@ -1,7 +1,11 @@
-import { _decorator, Component, Node, Vec3 } from 'cc';
+import { _decorator, Component, Node, Vec3, Quat, ModelComponent, SkinningModelComponent } from 'cc';
 const { ccclass, property } = _decorator;
 
 const delta = new Vec3();
+const view = new Vec3();
+const rot = new Quat();
+
+const textureCounts = [2, 4];
 
 @ccclass('Consumer')
 export class Consumer extends Component {
@@ -13,21 +17,35 @@ export class Consumer extends Component {
     // reference
     radius = 0;
 
+    model: ModelComponent = null;
+
     start () {
         this.radius = this.node.scene.getChildByName('Ground').scale.x * 0.5;
+        this.model = this.node.getComponentInChildren(SkinningModelComponent);
+        const tilingOffset = [1 / textureCounts[0], 1 / textureCounts[1]];
+        tilingOffset.push(Math.floor(Math.random() * textureCounts[0]) * tilingOffset[0]);
+        tilingOffset.push(Math.floor(Math.random() * textureCounts[1]) * tilingOffset[1]);
+        this.model.setInstancedAttribute('a_tiling_offset', tilingOffset);
     }
 
     update (deltaTime: number) {
         this.nextTurn -= deltaTime;
         if (this.nextTurn < 0) {
             this.velocity.set(Math.random() - 0.5, 0, Math.random() - 0.5).normalize().multiplyScalar(Math.random());
+            this._updateRotation();
             this.nextTurn = Math.random() * 10;
         }
         const pos = this.node.position;
         Vec3.add(delta, pos, Vec3.multiplyScalar(delta, this.velocity, deltaTime * 5));
-        if (delta.x < -this.radius || delta.x > this.radius) this.velocity.x = -this.velocity.x;
-        if (delta.y < -this.radius || delta.y > this.radius) this.velocity.y = -this.velocity.y;
-        if (delta.z < -this.radius || delta.z > this.radius) this.velocity.z = -this.velocity.z;
+        let needRotationUpdate = false;
+        if (delta.x < -this.radius || delta.x > this.radius) this.velocity.x = -this.velocity.x, needRotationUpdate = true;
+        if (delta.y < -this.radius || delta.y > this.radius) this.velocity.y = -this.velocity.y, needRotationUpdate = true;
+        if (delta.z < -this.radius || delta.z > this.radius) this.velocity.z = -this.velocity.z, needRotationUpdate = true;
+        if (needRotationUpdate) this._updateRotation();
         this.node.setPosition(delta);
+    }
+
+    _updateRotation () {
+        this.node.setRotation(Quat.fromViewUp(rot, Vec3.normalize(view, this.velocity)));
     }
 }

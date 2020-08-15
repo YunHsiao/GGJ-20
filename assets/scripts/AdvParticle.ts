cfa6b3d1e2eaebb0e12ce050bf02d3095690cf84
santy-wang <wh19930829@qq.com>
2020-08-15 20:20:03 +0800

    change particle logic

---
+++ assets/scripts/AdvParticle.ts
@@ -0,0 +1,30 @@
+import { _decorator, Component, Node, ParticleSystemComponent } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('AdvParticle')
+export class AdvParticle extends Component {
+
+    @property({type: [ParticleSystemComponent]})
+    public particles: ParticleSystemComponent[] = []
+    /* class member could be defined like this */
+    // dummy = '';
+
+    /* use `property` decorator if your want the member to be serializable */
+    // @property
+    // serializableDummy = 0;
+
+    start () {
+        // Your initialization goes here.
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+    stop () {
+        this.particles.forEach(x => x.stop());
+    }
+
+    play () {
+        this.particles.forEach(x => x.play());
+    }
+}
--- assets/scripts/AdvertisementController.ts
+++ assets/scripts/AdvertisementController.ts
@@ -1,35 +1,23 @@
 import { _decorator, Component, Node, ParticleSystemComponent, Vec3 } from 'cc';
 import { Advertisement } from './Advertisement';
+import { AdvParticle } from './AdvParticle';
 const { ccclass, property } = _decorator;
 
 @ccclass('AdvertisementController')
 export class AdvertisementController extends Component {
     public advertisementData: Advertisement;
-    @property({type: ParticleSystemComponent})
-    public adParticle: ParticleSystemComponent = null;
-    private _subParticles: ParticleSystemComponent[] = [];
+    @property({type: AdvParticle})
+    public adParticle: AdvParticle = null;
 
     private _curPassedTime: number = 0;
     private _showTime = 0;
 
-    start () {
-        for (let i = 0; i < this.adParticle.node.children.length; i++) {
-            let subNode = this.adParticle.node.children[i];
-            this._subParticles.push(subNode.getComponent(ParticleSystemComponent));
-        }
-    }
 
     show() {
-        const range = this.advertisementData.range;
-        //this.adParticle.node.setWorldScale(new Vec3(range, range, range));
-        this.adParticle.shapeModule.radius = range;
+        const range = this.advertisementData.range / 10;
+        this.adParticle.node.setScale(new Vec3(range, range, range));
         this.adParticle.stop();
         this.adParticle.play();
-        // this._subParticles.forEach((particle) => {
-        //     particle.startSizeX.constant = this.advertisementData.range;
-        //     particle.stop();
-        //     particle.play();
-        // });
 
 
         this._showTime = 2;

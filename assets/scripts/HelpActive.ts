3b5ef81cba283fac5546fa24dc07a2a8bdf595d5
Unknown <sun.cloud.mo@gmail.com>
2020-08-16 12:29:05 +0800

    update HelpUI

---
+++ assets/scripts/HelpActive.ts
@@ -0,0 +1,34 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('HelpActive')
+export class HelpActive extends Component {
+    /* class member could be defined like this */
+    // dummy = '';
+
+    /* use `property` decorator if your want the member to be serializable */
+    // @property
+    // serializableDummy = 0;
+    @property({type: Node})
+    public helpNode: Node = null;
+
+    start () {
+        // Your initialization goes here.
+        this.helpNode.active = true;
+        this.scheduleOnce(()=> {
+            this.helpNode.active = false;
+        }, 10);
+    }
+
+    visableChange() {
+        this.helpNode.active = !this.helpNode.active;
+    }
+
+    hide() {
+        this.helpNode.active = false;
+    }
+
+    // update (deltaTime: number) {
+    //     // Your update function goes here.
+    // }
+}
